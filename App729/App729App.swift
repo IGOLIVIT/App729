f4fda10bf0ec1f16d554c6d6915b0c8c41b09a77
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-07-24 16:59:54 +0300

    Initial Commit

---
+++ App729/App729App.swift
@@ -0,0 +1,17 @@
+//
+//  App729App.swift
+//  App729
+//
+//  Created by IGOR on 24/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct App729App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
